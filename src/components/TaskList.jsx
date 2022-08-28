e0063c955e274f5e92eeb6ba1f3a6551605c09d2
Sebastián <srojoa19@gmail.com>
2022-08-27 22:13:36 -0400

    Simplificación de código

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,17 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
-import Swal from 'sweetalert2'
-import { deleteTaskAPI, getTasksAPI } from '../store/features/tasks/thunks';
+import {  getTasksAPI } from '../store/features/tasks/thunks';
 import { TaskTable } from './TaskTable';
 import { useForm } from '../hooks/useForm';
-import { orderTasks } from '../store/features/tasks/taskSlice';
 import { changeEditCreate, changeOpenModal } from '../store/features/actions/actionSlice';
 import { ActionButton } from './ActionButton';
 import { TaskModal } from './TaskModal';
 import { Container } from '@mui/system';
-import { Button, Menu, MenuItem, Fade } from '@mui/material';
-import TestTable from './TestTable';
-import ExampleTable from './ExampleTable';
 
 export const TaskList = () => {
 
@@ -21,8 +16,6 @@ export const TaskList = () => {
         expirationDate: null,
     }); 
 
-    //Accedo a tasks del store, que a su vez, es el initialState
-    const { tasks, tasksSelected } = useSelector(state => state.tasks);
     const { openModal } = useSelector(state => state.actions);
     const dispatch = useDispatch();
 
@@ -45,113 +38,36 @@ export const TaskList = () => {
     }
 
 
-    const handleDeleteTask = () => {
-        Swal.fire({
-            title: '¿Está seguro?',
-            text: "No podrá volver a recuperar las tareas eliminadas",
-            icon: 'question',
-            showCancelButton: true, 
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Sí, borrarlas!',
-            cancelButtonText: "Cancelar"
-        }).then((result) => {
-            if (result.isConfirmed) {
-                dispatch(deleteTaskAPI(tasksSelected))
-                Swal.fire({
-                    position: 'center',
-                    icon: 'success',
-                    title: 'Eliminación correcta',
-                    showConfirmButton: false,
-                    timer: 1500
-                })
-            }
-        })
-    }
-
-
-    const [anchorEl, setAnchorEl] = useState(null);
-    const open = Boolean(anchorEl);
-    const handleOrderMenu = (order) => {
-        dispatch(orderTasks(order))
-        setAnchorEl(null);
-    };
-
 
     return (
         <Container maxWidth="md">
             <header>
                 <h1>Cosas por Hacer</h1>
                 <ol>
-                    <li>Impedir que se reorganize tabla al completar tarea (???)</li>
-                    <li>Añadir color de vencimiento</li>
+                    <li>VOLVER A REVISAR CÓDIGO DE TABLA A PROFUNDIDAD Y OPTIMIZAR</li>
+                    <li>Ordenar código. Esto hacerlo viendo el curso de react de fazt</li>
+                    <li>Añadir buscador de tareas (despues de tener código ordenado)</li>
                     <li>Ver la posibilidad de páginar desde backend y frontend con tabla.</li>
                 </ol>
             </header>
 
-            <div>
-                <ActionButton
-                    handleFunction={() => handleOpenModal(null)}
-                    text={`Crear tarea`}
-                    icon={`addTask`}
-                />
-
-                <Button
-                    variant="outlined"
-                    id="fade-button"
-                    aria-controls={open ? 'fade-menu' : undefined}
-                    aria-haspopup="true"
-                    aria-expanded={open ? 'true' : undefined}
-                    onClick={(event) => setAnchorEl(event.currentTarget)}
-                >
-                    Ordenar Tareas
-                </Button>
-                <Menu
-                    id="fade-menu"
-                    MenuListProps={{
-                        'aria-labelledby': 'fade-button',
-                    }}
-                    anchorEl={anchorEl}
-                    open={open}
-                    onClose={() => setAnchorEl(null)}
-                    TransitionComponent={Fade}
-                >
-                    <MenuItem onClick={() => handleOrderMenu('order1')}>Fecha de creación</MenuItem>
-                    <MenuItem onClick={() => handleOrderMenu('order2')}>Fecha de vencimiento</MenuItem>
-                    <MenuItem onClick={() => handleOrderMenu('order3')}>Estado</MenuItem>
-                    <MenuItem onClick={() => handleOrderMenu('order4')}>Descripción</MenuItem>
-                </Menu>
-            </div>
-
-
-
-
-
-
-            <TestTable 
-                handleOpenModal={handleOpenModal}
+  
+            <ActionButton
+                handleFunction={() => handleOpenModal(null)}
+                text={`Crear tarea`}
+                icon={`addTask`}
             />
 
-            {/* <ExampleTable /> */}
 
-           
-
-
-
-
-            <TaskTable
-                tasks={tasks}
+            <TaskTable 
                 handleOpenModal={handleOpenModal}
             />
-
 
             <TaskModal
                 handleChange={handleChange}
                 inputs={inputs}
                 setInputs={setInputs}
             />
-
-
             
         </Container>
     )
--- src/store/features/tasks/taskSlice.js
+++ src/store/features/tasks/taskSlice.js
@@ -12,9 +12,10 @@ export const taskSlice = createSlice({
         getTasks: (state, action) => {
             state.tasks  = action.payload.tasks.sort((a, b) => {
                 return ( a.expirationDate > b.expirationDate) ? 1 : -1
-            }).sort((a,b) => {
-                return (a.completed > b.completed) ? 1 : -1
             })
+            // .sort((a,b) => {
+                // return (a.completed > b.completed) ? 1 : -1
+            // })
             // for(let x in a)
             // a[x].completed === true  ? a.push( a.splice(x,1)[0] )  : 0;
 
@@ -35,31 +36,6 @@ export const taskSlice = createSlice({
             ? state.tasksSelected.push(action.payload) 
             : state.tasksSelected = state.tasksSelected.filter(task => task.id !== id)
         },
-        orderTasks: (state, action) => {
-            if(action.payload === 'order1'){ //Por fecha de creación
-                state.tasks = state.tasks.sort((a, b) => {
-                    return ( a.createdAt > b.createdAt) ? 1 : -1
-                }) 
-            }
-            if(action.payload === 'order2'){ //Por fecha de vencimiento
-                state.tasks = state.tasks.sort((a, b) => {
-                    return ( a.expirationDate > b.expirationDate) ? 1 : -1
-                }) 
-            }
-            if(action.payload === 'order3'){ //Por estado
-                state.tasks = state.tasks.sort((a, b) => {
-                    return ( a.completed > b.completed) ? 1 : -1
-                }) 
-            } 
-            if(action.payload === 'order4'){ //Por texto
-                state.tasks = state.tasks.sort((a, b) => {
-                    return (a.description.toLowerCase() > b.description.toLowerCase()) ? 1: -1
-                })
-            } 
-            // state.tasks = state.tasks.sort((a,b) => {
-            //     return (a.completed > b.completed) ? 1 : -1
-            // })
-        },
         selectedAllTasks: (state, action) => {;
             state.tasksSelected = []
             state.tasks.forEach(task => {
@@ -78,5 +54,5 @@ export const taskSlice = createSlice({
     }
 })
 
-export const { getTasks, addTask, deleteTask, updateTask, changeOpenModal, changeEditCreate, handleSelectedTask, orderTasks, selectedAllTasks,removeAllTasks, deselectedTasks } = taskSlice.actions  
+export const { getTasks, addTask, deleteTask, updateTask, changeOpenModal, changeEditCreate, handleSelectedTask, selectedAllTasks,removeAllTasks, deselectedTasks } = taskSlice.actions  
 export default taskSlice.reducer
